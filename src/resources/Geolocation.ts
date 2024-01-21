a3fbbe46067cd0b0c640cac1326b200e4f22a249
Jefferson Calmon <jeffjeff0197@gmail.com>
2024-01-20 23:14:24 -0300

    :hammer: refactor(geolocation): Fix a little simple bug

--- src/resources/Geolocation.ts
+++ src/resources/Geolocation.ts
@@ -1,11 +1,20 @@
 import * as T from '../types/geolocation';
 
 export async function geolocation() {
+	/**
+	 * https://api.sefinek.net/api/v2/geoip/209.14.149.25
+	 * http://ip-api.com/json?fields=66846719
+	 */
+
 	try {
-		const ipResponse = await fetch('https://api.myip.com/');
+		const ipResponse = await fetch('https://api.myip.com/', {
+			mode: 'no-cors',
+		});
 		const { ip }: T.MyIpResponse = await ipResponse.json();
 
-		const geoResponse = await fetch(`https://ipapi.co/${ip}/json`);
+		const geoResponse = await fetch(`https://ipapi.co/${ip}/json`, {
+			mode: 'no-cors',
+		});
 		const data: T.IpApiResponse = await geoResponse.json();
 
 		return data;
